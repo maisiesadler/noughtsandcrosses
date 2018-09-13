2dc8a05106a0fc525e724ffb5f9d59312f63d016
Maisie Sadler <maisiesadler@Maisies-MacBook-Pro.local>
2018-09-13 20:23:46 +0100

    remove unused files

--- models.go
+++ models.go
@@ -1,13 +1,7 @@
 package main
 
-type joinGameRequest struct {
-	GameName   string `json:"gameName"`
-	PlayerName string `json:"playerName"`
-}
-
 type placeRequest struct {
-	GameName string `json:"gameName"`
-	Counter  string `json:"counter"`
-	X        int    `json:"x"`
-	Y        int    `json:"y"`
+	Counter string `json:"counter"`
+	X       int    `json:"x"`
+	Y       int    `json:"y"`
 }
