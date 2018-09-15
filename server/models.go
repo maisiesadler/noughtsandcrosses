eca063512d04b7ecf1ba326aefd1611b406d87fe
Maisie Sadler <maisiesadler@Maisies-MacBook-Pro.local>
2018-09-15 08:44:09 +0100

    move server code into separate package

--- server/models.go
+++ server/models.go
@@ -1,4 +1,4 @@
-package main
+package server
 
 type placeRequest struct {
 	Counter string `json:"counter"`
---
+++ server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/maisiesadler/noughtsandcrosses/game"
+)
+
+// StartServer starts the server
+func StartServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s", game.String())
+	})
+
+	http.HandleFunc("/place", func(w http.ResponseWriter, r *http.Request) {
+		var request placeRequest
+		readBody(w, r, &request)
+		err := game.PlaceCounter(request.Counter, request.X, request.Y)
+		if err != nil {
+			log.Printf("Error placing counter: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, game.String())
+	})
+
+	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		game.Reset()
+		fmt.Fprintf(w, "ok")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
